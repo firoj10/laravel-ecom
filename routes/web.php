18c3e770dce1bc4027dd7312866f4a404a31e7d4
firoj10 <mdfirojhasan.info@gmail.com>
2023-11-07 00:13:35 +0600

    ecom create projects

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\User\UserController;
+use App\Http\Controllers\Admin\AdminController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('admin/dashboard',[AdminController::class, 'admin'])->name('admin.dashboard');
+});
+
+Route::middleware(['auth', 'user'])->group(function () {
+    Route::get('user/dashboard',[UserController::class, 'user'])->name('user.dashboard');
+});
